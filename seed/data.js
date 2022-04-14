e9772313025ccd7634cb38c7d9117d00e225ddc5
Stacey Csikos <stacey@Staceys-MacBook-Pro.local>
2022-04-14 08:57:05 -0400

    seeded db

--- seed/data.js
+++ seed/data.js
@@ -1,23 +1,12 @@
 import db from "../db/connection.js";
-import Monster from "../models/monsters.js";
-import monsters from "./monsters.json" assert { type: "json" };
+import Gym from "../models/gym.js";
+import gyms from "./gyms.json" assert { type: "json" };
 
 const insertData = async () => {
   //reset db
   db.dropDatabase();
-  let myMonsters = [];
 
-  for (let i = 0; i < monsters.length; i++) {
-    myMonsters.push({
-      name: monsters[i].name,
-      description: monsters[i].description,
-      type: monsters[i].type,
-      species: monsters[i].species,
-    });
-  }
-
-  console.log(myMonsters);
-  await Monster.insertMany(myMonsters);
+  await Gym.insertMany(gyms);
 
   db.close();
 };
